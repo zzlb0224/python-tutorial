343a4ed6956e115bb8299c9786248bfe01f4a883
周利斌@书香门第 <125377187@qq.com>
2019-12-04 16:54:21 +0800

    a

--- c08form/front/views.py
+++ c08form/front/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .forms import MessageBoardForm
 # Create your views here.
+
+
+class IndexView(View):
+    def get(self, request):
+        form = MessageBoardForm()
+        return render(request, 'index.html', context={'form': form})
+
+    def post(self, request):
+        form = MessageBoardForm(request.POST)
+        if form.is_valid():
+            return render(request, 'index.html', context={'form': form, 'message': "ok"})
+        return render(request, 'index.html', context={'form': form, 'message': form.errors})
