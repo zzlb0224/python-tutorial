ab33f8d1f4f5cd9031f1caf1dd610494b8bbaf6e
周利斌@书香门第 <125377187@qq.com>
2019-12-06 13:04:51 +0800

    1

--- c09orm/book/models.py
+++ c09orm/book/models.py
@@ -5,10 +5,10 @@ from django.utils.timezone import timezone, now
 
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100, null=False)
-    description = models.CharField(max_length=100, null=True)
-    author = models.CharField(max_length=100, null=True)
-    price = models.FloatField(default=0, null=False)
+    title = models.CharField(max_length=100, blank=True)
+    description = models.CharField(max_length=100, blank=True)
+    author = models.CharField(max_length=100, blank=True)
+    price = models.FloatField(default=0, blank=True)
 
     def __str__(self):
         return "book:{{id:{id},title:{title}}}".format(title=self.title,
@@ -18,12 +18,27 @@ class Book(models.Model):
 
 class Publisher(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=False)
-    address = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
 
 
 class Article(models.Model):
     id = models.AutoField(primary_key=True)
     removed = models.BooleanField(default=False)
     create_time = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, blank=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=40, blank=True)
+    signature = models.TextField(blank=True)
+    avastar = models.URLField(blank=True)
+
+
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    create_time = models.DateTimeField(auto_now_add=True)
