d3610a25d9a645938dfca9ee5907b48bcea19d05
周利斌@书香门第 <125377187@qq.com>
2019-12-06 14:56:01 +0800

    fixed

--- c09orm/fk/urls.py
+++ c09orm/fk/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = 'fk'
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('category/add/<name>/', views.addcategory),
+    path('article/add/', views.addarticle)
 ]
--- c09orm/fk/views.py
+++ c09orm/fk/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
 # Create your views here.
 
 
 def index(request):
+    print(Article.objects.all())
+    return HttpResponse('ok')
+
+
+def addcategory(request, name):
+    Category(name=name).save()
+    return HttpResponse('ok')
+
+
+def category(request):
+    cas = Category.objects.all()
+    return HttpResponse(cas)
+
+
+def addarticle(request):
+    category = Category.objects.first()
+    article = Article(title='abc', content='111')
+    article.category = category
+    article.save()
     return HttpResponse('ok')
