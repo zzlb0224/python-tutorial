10e8007602684516430163c81af586c94017a059
周利斌@书香门第 <125377187@qq.com>
2019-12-07 14:36:29 +0800

    fixed

--- c10query/front/models.py
+++ c10query/front/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+
+    def __str__(self):
+        return r"article:{title:%s}" % self.title
